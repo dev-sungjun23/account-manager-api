d886cd8ded7836fee90e2b462fded7eb527ec67d
sungjun <dbtjdwns.dev@gmail.com>
2025-05-15 17:48:52 +0900

    chore: 초기 커밋 - REST API 계좌 관리 프로젝트 생성

---
+++ src/main/java/com/example/account_manager_api/AccountManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.account_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagerApiApplication.class, args);
+	}
+
+}
